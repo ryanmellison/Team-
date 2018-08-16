adca7a7e0afe76779ce91ee313551123c0f2cd56
Unknown <ABarraganEscobell@student.neumont.edu>
2018-08-16 17:48:53 -0600

    Revert "autocommit"
    This reverts commit 716b8b27fe84e8f1e8265a4fc7852f8e70192c9d.

--- DealOrNoDeal/DealOrNoDeal/DealOrNoDeal/Game.xaml.cs
+++ DealOrNoDeal/DealOrNoDeal/DealOrNoDeal/Game.xaml.cs
@@ -5,7 +5,6 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.Serialization;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
